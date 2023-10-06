6d4389c86a82055e97db149b6961fd2d90fe729c
hoangvh238 <hoangvh238.dev@gmail.com>
2023-10-06 23:24:20 +0700

    up

--- src/hooks/userStorage.ts
+++ src/hooks/userStorage.ts
@@ -1,27 +1,10 @@
 "use client"
 
-type UserBase = {
-  userName: string,
-  role: string,
-};
 
 
 
 export const storage = async () => {
-  // const token = await getCookie("token");
 
-  // if (!token) return null;
-  // const dispatch = useDispatch();
-  // var decoded: UserBase = jwt_decode(token);
-  // const user = {
-  //   userName: decoded!.userName,
-  //   role: decoded!.role,
-  // };
-  // dispatch(
-  //   store({
-  //     user
-  //   })
-  // );
 }
 
 export const getUser = async () => { 
